16de2fd98c8bc6601b7445b49b7b615eb76cb70c
Owen Voke <owen.voke@cylix.co.uk>
2018-09-04 12:56:26 +0100

    Change to use Git.io short urls

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@
 export LARAVEL_DIRECTORY='src/laravel'
 
 # Download the latest IDE Helper/Meta files
-wget https://gist.githubusercontent.com/barryvdh/5227822/raw/_ide_helper.php -O ${LARAVEL_DIRECTORY}/_ide_helper.php
-wget https://gist.githubusercontent.com/barryvdh/bb6ffc5d11e0a75dba67/raw/.phpstorm.meta.php -O ${LARAVEL_DIRECTORY}/.phpstorm.meta.php
+wget https://git.io/fA4pE -O ${LARAVEL_DIRECTORY}/_ide_helper.php
+wget https://git.io/fA4pz -O ${LARAVEL_DIRECTORY}/.phpstorm.meta.php
 
 # ZIP the source files to a JAR
 cd src
