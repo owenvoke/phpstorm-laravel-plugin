b0d809e36af7f9bec43e692a4bcb4411b2193a82
Owen Voke <owen.voke@cylix.co.uk>
2018-09-04 12:42:13 +0100

    Update the build script to download the Gists

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,7 +2,7 @@
 
 All notable changes to `phpstorm-laravel-plugin` will be documented in this file.
 
-Updates should follow the [Keep a CHANGELOG](http://keepachangelog.com/) principles.
+Updates should follow the [Keep a CHANGELOG](https://keepachangelog.com) principles.
 
 ## v1.0.1 - 2017-11-24
 
--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+
+# Download the latest IDE Helper/Meta files
+wget https://gist.githubusercontent.com/barryvdh/5227822/raw/_ide_helper.php -O laravel/_ide_helper.php
+wget https://gist.githubusercontent.com/barryvdh/bb6ffc5d11e0a75dba67/raw/.phpstorm.meta.php -O laravel/.phpstorm.meta.php
+
+# ZIP the source files to a JAR
 cd src
 zip -r ../phpstorm-library-plugin.jar *
 cd ..
